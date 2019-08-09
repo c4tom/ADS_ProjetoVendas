c13f04c2d14b6855834d64f5a65980aff83f43ec
Candido H Tominaga <candido1212@gmail.com>
2019-08-09 04:05:18 -0300

    Equals e ToString

--- VendasConsole/Cliente.cs
+++ VendasConsole/Cliente.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,6 +22,32 @@ namespace VendasConsole
         public string Nome { get; set; }
         public string Cpf { get; set; }
         public DateTime CriadoEm { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            Cliente o = (Cliente) obj;
+            if(o.Nome.Equals(Nome) && o.Cpf.Equals(Cpf))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            Type objType = this.GetType();
+            PropertyInfo[] propertyInfoList = objType.GetProperties();
+            StringBuilder result = new StringBuilder();
+            foreach (PropertyInfo propertyInfo in propertyInfoList)
+                result.AppendFormat("{0}={1} ", propertyInfo.Name, propertyInfo.GetValue(this));
+
+            return result.ToString();
+        }
         #endregion
 
 
