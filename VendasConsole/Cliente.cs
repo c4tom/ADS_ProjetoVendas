f044ce85a1c1f6761a67fe92113bf2be2ce2f479
Candido H Tominaga <candido1212@gmail.com>
2019-08-09 03:24:33 -0300

    Construtores com parametro e sem

--- VendasConsole/Cliente.cs
+++ VendasConsole/Cliente.cs
@@ -9,8 +9,18 @@ namespace VendasConsole
     class Cliente
     {
         #region C#
+        public Cliente() {
+            CriadoEm = DateTime.Now;
+        }
+
+        public Cliente(string cpf)
+        {
+            CriadoEm = DateTime.Now;
+            Cpf = cpf;
+        }
         public string Nome { get; set; }
         public string Cpf { get; set; }
+        public DateTime CriadoEm { get; set; }
         #endregion
 
 
